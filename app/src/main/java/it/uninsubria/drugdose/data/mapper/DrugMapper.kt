53cdb907f520a65f77659fba1cf1c00bab5a3042
AmStanDem <96748673+AmStanDem@users.noreply.github.com>
2026-06-06 00:59:26 +0200

    feat(data): add DrugMapper.kt

---
+++ app/src/main/java/it/uninsubria/drugdose/data/mapper/DrugMapper.kt
@@ -0,0 +1,38 @@
+package it.uninsubria.drugdose.data.mapper
+
+import it.uninsubria.drugdose.data.local.entity.DrugEntity
+import it.uninsubria.drugdose.domain.model.Drug
+
+// Trasforma Entity -> Domain
+fun DrugEntity.toDomain(): Drug {
+    return Drug(
+        id = id,
+        name = name,
+        indication = indication,
+        formulaType = formulaType,
+        unitDose = unitDose,
+        unit = unit,
+        maxDose = maxDose,
+        minWeightKg = minWeightKg,
+        minAgeYears = minAgeYears,
+        source = source,
+        alerts = alerts
+    )
+}
+
+// Trasforma Domain -> Entity (utile per il salvataggio)
+fun Drug.toEntity(): DrugEntity {
+    return DrugEntity(
+        id = id,
+        name = name,
+        indication = indication,
+        formulaType = formulaType,
+        unitDose = unitDose,
+        unit = unit,
+        maxDose = maxDose,
+        minWeightKg = minWeightKg,
+        minAgeYears = minAgeYears,
+        source = source,
+        alerts = alerts
+    )
+}
